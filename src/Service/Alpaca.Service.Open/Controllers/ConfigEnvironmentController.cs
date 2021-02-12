243efc791cd1e6893145f5ff04c8f13c0a45c926
doddgu <358683537@qq.com>
2021-02-12 22:07:25 +0800

    #3 add auth

--- src/Service/Alpaca.Service.Open/Controllers/ConfigEnvironmentController.cs
+++ src/Service/Alpaca.Service.Open/Controllers/ConfigEnvironmentController.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Alpaca.Service.Open.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [EnableCors("AllowCors")]
     [Route("api/[controller]")]
     [ApiController]
